748837e9ff12ea99975f2f601825ef4cbc71be0a
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2025-06-07 03:14:59 -0300

    feat: add favicon to website layout
    Include favicon link in the head section of the layout to display the site icon in browser tabs

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${roboto.variable} antialiased`}>{children}</body>
     </html>
   )
